c113c5a7de273535c0edc97319bc3d0dafdddd47
ronow2cn <594115727@qq.com>
2017-08-09 17:12:53 +0800

    auth.1.2.1

--- src/auth/route/authweixin.go
+++ src/auth/route/authweixin.go
@@ -50,43 +50,67 @@ func HandlerWeiXinAuth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//auth type: default token auth
+	err := Err.OK
 	authType := r.PostFormValue("authtype")
-	openid := r.PostFormValue("uid")
-
 	if authType != comm.I32toa(macrocode.LoginType_WeiXinCode) { //token auth
-		processWeiXinTokenAuth(w, openid, token)
+		openid := r.PostFormValue("uid")
+		err = processWeiXinTokenAuth(w, openid, token)
 	} else { //code auth
-		processWeiXinCodeAuth(w, token)
+		err = processWeiXinCodeAuth(w, token)
+	}
+
+	if err != Err.OK {
+		makeAuthRes(w, &AuthRes{Result: int32(err)})
 	}
 }
 
-func processWeiXinTokenAuth(w http.ResponseWriter, openid string, token string) {
+func processWeiXinTokenAuth(w http.ResponseWriter, openid string, token string) int {
+	account := dbmgr.CenterGetAccountInfo(macrocode.ChannelType_WeiXin, openid)
+	if account == nil {
+		log.Error("processWeiXinTokenAuth get account failed")
+		return Err.Failed
+	}
 
+	if token != account.Token {
+		log.Error("processWeiXinTokenAuth token error")
+		return Err.Failed
+	}
+
+	if time.Now().After(account.ExpireT) {
+		err := updateWeinXinTokenByRefreshToken(w, account.RefreshToken)
+		return err
+	} else {
+		makeAuthRes(w, &AuthRes{
+			Result: Err.OK,
+			OpenId: openid,
+			Token:  token,
+			Expire: account.ExpireT.Unix(),
+		})
+	}
+
+	return Err.OK
 }
 
-func processWeiXinCodeAuth(w http.ResponseWriter, code string) {
-	url := fmt.Sprintf("%s?appid=%s&secret=%s&code=%s&grant_type=authorization_code",
-		config.Auth.WeiXin.TokenUrl, config.Auth.WeiXin.AppId, config.Auth.WeiXin.AppKey, code)
+func updateWeinXinTokenByRefreshToken(w http.ResponseWriter, refrtoken string) int {
+	url := fmt.Sprintf("%s?appid=%s&grant_type=refresh_token&refresh_token=%s",
+		config.Auth.WeiXin.RefrUrl, config.Auth.WeiXin.AppId, refrtoken)
 
 	ret, err := comm.HttpGetT(url, HttpTimeOutSecond)
 	if err != nil {
-		log.Error("processWeiXinCodeAuth HttpGetT error", err)
-		makeAuthRes(w, &AuthRes{Result: Err.Failed})
-		return
+		log.Error("updateWeinXinTokenByRefreshToken HttpGetT error", err)
+		return Err.Failed
 	}
 
 	var jret weixinTokenRet
 	err = json.Unmarshal([]byte(ret), &jret)
 	if err != nil {
 		log.Error("processWeiXinCodeAuth Unmarshal error", err)
-		makeAuthRes(w, &AuthRes{Result: Err.Failed})
-		return
+		return Err.Failed
 	}
 
 	if len(jret.ErrCode) != 0 {
 		log.Error("processWeiXinCodeAuth ErrCode", jret.ErrCode, jret.ErrMsg)
-		makeAuthRes(w, &AuthRes{Result: comm.Atoi32(jret.ErrCode)})
-		return
+		return Err.Failed
 	}
 
 	makeAuthRes(w, &AuthRes{
@@ -100,6 +124,45 @@ func processWeiXinCodeAuth(w http.ResponseWriter, code string) {
 	asyncop.Push(func() {
 		dbmgr.CenterUpdateAccountInfo(macrocode.ChannelType_WeiXin, jret.OpenId, jret.Token, jret.RefrToken, jret.ExpireIn)
 	}, nil)
+
+	return Err.OK
+}
+
+func processWeiXinCodeAuth(w http.ResponseWriter, code string) int {
+	url := fmt.Sprintf("%s?appid=%s&secret=%s&code=%s&grant_type=authorization_code",
+		config.Auth.WeiXin.TokenUrl, config.Auth.WeiXin.AppId, config.Auth.WeiXin.AppKey, code)
+
+	ret, err := comm.HttpGetT(url, HttpTimeOutSecond)
+	if err != nil {
+		log.Error("processWeiXinCodeAuth HttpGetT error", err)
+		return Err.Failed
+	}
+
+	var jret weixinTokenRet
+	err = json.Unmarshal([]byte(ret), &jret)
+	if err != nil {
+		log.Error("processWeiXinCodeAuth Unmarshal error", err)
+		return Err.Failed
+	}
+
+	if len(jret.ErrCode) != 0 {
+		log.Error("processWeiXinCodeAuth ErrCode", jret.ErrCode, jret.ErrMsg)
+		return Err.Failed
+	}
+
+	makeAuthRes(w, &AuthRes{
+		Result: Err.OK,
+		OpenId: jret.OpenId,
+		Token:  jret.Token,
+		Expire: jret.ExpireIn + time.Now().Unix(),
+	})
+
+	//flush
+	asyncop.Push(func() {
+		dbmgr.CenterUpdateAccountInfo(macrocode.ChannelType_WeiXin, jret.OpenId, jret.Token, jret.RefrToken, jret.ExpireIn)
+	}, nil)
+
+	return Err.OK
 }
 
 func makeAuthRes(w http.ResponseWriter, ret *AuthRes) {
